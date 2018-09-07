1b769a73f8a5fe1b45cd2939bb1e4e92cf4c1069
Kyle Miller <kylelogan4@yahoo.com>
2018-09-06 22:12:01 -0400

    separation of client and server side

--- project1/src/dto/sql-user.ts
+++
@@ -1,9 +0,0 @@
-export class SqlUser {
-  ers_users_id = 0;
-  ers_username = '';
-  ers_password = '';
-  user_first_name = '';
-  user_last_name = '';
-  user_email = '';
-  ers_role_id = '';
-}
--- project1/src/util/reim-converter.ts
+++
@@ -1,9 +0,0 @@
-import { Reim } from "../model/reim";
-import { SqlReim } from "../dto/sql-reim";
-
-/**
- * This is used to convert a sql reimbursement into an actual reimbursement
- */
-export function reimConverter(reim: SqlReim) {
-  return new Reim(reim.reimb_id, reim.reimb_amount, reim.reimb_submitted, reim.reimb_resolved, reim.reimb_description, reim.reimb_author, reim.reimb_resolver, reim.reimb_status_id,  undefined);
-}
